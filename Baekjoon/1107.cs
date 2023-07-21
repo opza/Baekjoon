b61963b6b231c40db08e36667d4d3de283b23ea5
opza <mingu991114@gmail.com>
2023-07-21 09:51:52 +0900

    20230721

--- Baekjoon/1107.cs
+++ Baekjoon/1107.cs
@@ -1,4 +1,171 @@
-﻿const int CHANNEL_MAX = 500000;
+﻿#region 1
+//const int CHANNEL_MAX = 500000;
+//const int CURRENT_CHANNEL = 100;
+
+//using var rs = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+//using var ws = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+//var n = int.Parse(rs.ReadLine());
+//var m = int.Parse(rs.ReadLine());
+//var brokenButtons = m > 0 ? rs.ReadLine().Split().Select(int.Parse).ToArray() : new int[0];
+
+//ws.WriteLine(GetPressCount(n, brokenButtons));
+
+//int GetPressCount(int n, int[] brokenButtons)
+//{
+//    var unbrokenButtons = Enumerable.Range(0, 10).Except(brokenButtons).ToArray();
+//    var nLength = n <= 0 ? 1 : (int)Math.Log10(n) + 1;
+
+//    var pressCountWithChangedChannel = GetPressCountWithChangedChannel(n, nLength, 0, 0, unbrokenButtons);
+//    var pressCountWithCurrentChannel = GetPressCountWithCurrentChannel(n);
+
+//    return Math.Min(pressCountWithChangedChannel, pressCountWithCurrentChannel);
+//}
+
+//int GetPressCountWithChangedChannel(int n, int nLength, int value, int digit, int[] unbrokenButtons)
+//{
+//    if (digit > nLength)
+//        return CalculatePressCount(n, value);
+
+//    var nextDigit = digit + 1;
+//    var pressCountMin = int.MaxValue;
+
+//    foreach (var unbrokenButton in unbrokenButtons)
+//    {
+//        var currValue = value + unbrokenButton * (int)Math.Pow(10, digit);
+
+//        var nextPressCount = GetPressCountWithChangedChannel(n, nLength, currValue, nextDigit, unbrokenButtons);
+//        var currPressCount = CalculatePressCount(n, currValue);
+
+//        pressCountMin = Math.Min(pressCountMin, Math.Min(nextPressCount, currPressCount));
+//    }
+
+//    return pressCountMin;
+//}
+
+
+//int GetPressCountWithCurrentChannel(int n) => Math.Abs(n - CURRENT_CHANNEL);
+
+//int CalculatePressCount(int n, int value)
+//{
+//    if (value <= 0)
+//        return Math.Abs(n - value) + 1;
+
+//    return Math.Abs(n - value) + (int)Math.Log10(value) + 1;
+//}
+
+#endregion
+#region 2
+//const int INIT_CHANNEL = 100;
+
+//using var rs = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+//using var ws = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+//var n = int.Parse(rs.ReadLine());
+//var m = int.Parse(rs.ReadLine());
+//var brokenButtons = m > 0 ? rs.ReadLine().Split().Select(int.Parse).ToArray() : new int[0];
+
+//ws.WriteLine(GetPressCount(n, brokenButtons));
+
+//int GetPressCount(int n, int[] brokenButtons)
+//{
+//    var pressCount = Math.Abs(n - INIT_CHANNEL);
+//    var digitN = GetDigit(n);
+//    var maxChannel = Math.Pow(10, digitN + 1);
+
+//    for (int i = 0; i < maxChannel; i++)
+//    {
+//        if (ContainsBrokenButton(i, brokenButtons))
+//            continue;
+
+//        var currPressCount = Math.Abs(n - i) + GetDigit(i);
+//        pressCount = Math.Min(pressCount, currPressCount);
+//    }
+
+//    return pressCount;
+//}
+
+//bool ContainsBrokenButton(int value, int[] brokenButtons)
+//{
+//    if (value == 0)
+//        return brokenButtons.Contains(value);
+
+//    while (value > 0)
+//    {
+//        var numOfEnd = value % 10;
+//        if (brokenButtons.Contains(numOfEnd))
+//            return true;
+
+//        value /= 10;
+//    }
+
+//    return false;
+//}
+
+//int GetDigit(int value) => value <= 0 ? 1 : (int)Math.Log10(value) + 1;
+#endregion
+#region 3
+
+//const int INIT_CHANNEL = 100;
+
+//using var rs = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+//using var ws = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+//var n = int.Parse(rs.ReadLine());
+//var m = int.Parse(rs.ReadLine());
+//var brokenButtons = m > 0 ? rs.ReadLine().Split().Select(int.Parse).ToArray() : new int[0];
+//var brokenStates = new bool[10];
+//foreach (var brokenButton in brokenButtons)
+//{
+//    brokenStates[brokenButton] = true;
+//}
+
+//ws.WriteLine(GetPressCount(n, brokenStates));
+
+
+//int GetPressCount(int n, bool[] brokenStates)
+//{
+//    var pressCount = Math.Abs(n - INIT_CHANNEL);
+//    var digitN = GetDigit(n);
+//    var maxChannel = Math.Pow(10, digitN + 1);
+
+//    var currDigit = 1;
+//    var digitThresholds = 10;
+//    for (int i = 0; i < maxChannel; i++)
+//    {
+//        if (i >= digitThresholds)
+//        {
+//            currDigit++;
+//            digitThresholds *= 10;
+//        }
+
+//        if (ContainsBrokenButton(i, brokenStates))
+//            continue;
+
+//        var currPressCount = Math.Abs(n - i) + currDigit;
+//        pressCount = Math.Min(pressCount, currPressCount);     
+//    }
+
+//    return pressCount;
+//}
+
+//bool ContainsBrokenButton(int value, bool[] brokenStates)
+//{
+//    if (value == 0)
+//        return brokenStates[value];
+
+//    while (value > 0)
+//    {
+//        var numOfEnd = value % 10;
+//        if (brokenStates[numOfEnd])
+//            return true;
+
+//        value /= 10;
+//    }
+
+//    return false;
+//}
+
+//int GetDigit(int value) => value <= 0 ? 1 : (int)Math.Log10(value) + 1;
+#endregion
+#region 4
 const int CURRENT_CHANNEL = 100;
 
 using var rs = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
@@ -8,9 +175,6 @@ var m = int.Parse(rs.ReadLine());
 var brokenButtons = m > 0 ? rs.ReadLine().Split().Select(int.Parse).ToArray() : new int[0];
 
 ws.WriteLine(GetPressCount(n, brokenButtons));
-
-//var unbrokenButtons = Enumerable.Range(0, 10).Except(brokenButtons).ToArray();
-//ws.WriteLine(Test(n, 0, 0, unbrokenButtons));
 
 int GetPressCount(int n, int[] brokenButtons)
 {
@@ -26,7 +190,7 @@ int GetPressCount(int n, int[] brokenButtons)
 int GetPressCountWithChangedChannel(int n, int nLength, int value, int digit, int[] unbrokenButtons)
 {
     if (digit > nLength)
-        return CalculatePressCount(n, value);
+        return CalculatePressCount(n, value, digit);
 
     var nextDigit = digit + 1;
     var pressCountMin = int.MaxValue;
@@ -36,7 +200,7 @@ int GetPressCountWithChangedChannel(int n, int nLength, int value, int digit, in
         var currValue = value + unbrokenButton * (int)Math.Pow(10, digit);
 
         var nextPressCount = GetPressCountWithChangedChannel(n, nLength, currValue, nextDigit, unbrokenButtons);
-        var currPressCount = CalculatePressCount(n, currValue);
+        var currPressCount = CalculatePressCount(n, currValue, nextDigit);
 
         pressCountMin = Math.Min(pressCountMin, Math.Min(nextPressCount, currPressCount));
     }
@@ -47,108 +211,11 @@ int GetPressCountWithChangedChannel(int n, int nLength, int value, int digit, in
 
 int GetPressCountWithCurrentChannel(int n) => Math.Abs(n - CURRENT_CHANNEL);
 
-int CalculatePressCount(int n, int value)
+int CalculatePressCount(int n, int value, int valueLength)
 {
     if (value <= 0)
         return Math.Abs(n - value) + 1;
 
-    return Math.Abs(n - value) + (int)Math.Log10(value) + 1;
+    return Math.Abs(n - value) + valueLength;
 }
-
-(int, int) Test(int n, int currValue, int currDigit, int[] unbrokenButtons)
-{
-    var digitMaxValue = currDigit > 0 ? (int)Math.Pow(10, currDigit) : 0;
-    if (digitMaxValue / 10 >= n)
-    {
-        //if(digitMaxValue / 10 >= n)
-        //    return (Math.Abs(n - currValue) + currDigit, currValue);
-
-        var topValue = (Math.Abs(n - currValue) + currDigit, currValue);
-        var bottomValue = (Math.Abs(n - currValue % (digitMaxValue / 10)) + currDigit - 1, currValue % (digitMaxValue / 10));
-
-        return topValue.Item1 < bottomValue.Item1 ? topValue : bottomValue;
-    }
-
-    var pressCount = (int.MaxValue, int.MaxValue);
-
-    foreach (var unbrokenButton in unbrokenButtons)
-    {
-        var nextValue = currValue + unbrokenButton * (int)Math.Pow(10, currDigit);
-
-        var nextDigit = currDigit + 1;
-        var val = Test(n, nextValue, nextDigit, unbrokenButtons);
-        pressCount = val.Item1 < pressCount.Item1 ? val : pressCount;
-    }
-
-    return pressCount;
-}
-
-
-//ws.WriteLine(GetPressCount(n, brokenButtons));
-
-//int GetPressCount(int n, int[] brokenButtons)
-//{
-//    var pressCountWithChangedChannel = GetPressCountWithChangedChannel(n, brokenButtons);
-//    var pressCountWithCurrentChannel = GetPressCountWithCurrentChannel(n);
-
-//    if (pressCountWithChangedChannel < 0)
-//        return pressCountWithCurrentChannel;
-
-//    return Math.Min(pressCountWithChangedChannel, pressCountWithCurrentChannel);
-//}
-
-//int GetPressCountWithChangedChannel(int n, int[] brokenButtons)
-//{
-//    var pressCount = 0;
-//    var oriN = n;
-//    var nearbyCh = 0;
-//    var unbrokenButtons = Enumerable.Range(0, 10).Except(brokenButtons).ToArray();
-
-//    var currDigit = 1;
-//    while (n > 0)
-//    {
-//        var target = n % 10;
-//        var nearbyValue = GetNearestValue(target, unbrokenButtons);
-//        if (nearbyValue < 0)
-//            return -1;
-
-//        nearbyCh += nearbyValue * currDigit;
-
-//        n /= 10;
-//        currDigit *= 10;
-//        pressCount++;
-//    }
-
-//    pressCount += Math.Abs(oriN - nearbyCh);
-
-//    return pressCount;
-//}
-
-//int GetPressCountWithCurrentChannel(int n) => Math.Abs(n - CURRENT_CHANNEL);
-
-//int GetNearestValue(int target, int[] unbrokenButtons)
-//{
-//    if (unbrokenButtons.Length <= 0)
-//        return -1;
-
-//    if (unbrokenButtons.Length == 1)
-//        return unbrokenButtons[0];
-
-//    if (target <= unbrokenButtons[0])
-//        return unbrokenButtons[0];
-
-//    if (target >= unbrokenButtons[unbrokenButtons.Length - 1])
-//        return unbrokenButtons[unbrokenButtons.Length - 1];
-
-//    for (int i = 0; i < unbrokenButtons.Length - 1; i++)
-//    {
-//        var lastValue = unbrokenButtons[i];
-//        var nextValue = unbrokenButtons[i + 1];
-
-//        if (target >= lastValue && target <= nextValue)
-//            return target - lastValue <= nextValue - target ? lastValue : nextValue;
-//    }
-
-//    return -1;
-//}
-
+#endregion
---
+++ Baekjoon/1764.cs
@@ -0,0 +1,27 @@
+﻿using var rs = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+using var ws = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+var inputs = rs.ReadLine().Split().Select(int.Parse).ToArray();
+var n = inputs[0];
+var m = inputs[1];
+
+var groupA = new string[n];
+for (int i = 0; i < n; i++)
+{
+    groupA[i] = rs.ReadLine();
+}
+
+var groupB = new string[m];
+for (int i = 0; i < m; i++)
+{
+    groupB[i] = rs.ReadLine();
+}
+
+var intersectedGroup = groupA.Intersect(groupB).ToArray();
+Array.Sort(intersectedGroup);
+
+ws.WriteLine(intersectedGroup.Length);
+
+foreach (var member in intersectedGroup)
+{
+    ws.WriteLine(member);
+}

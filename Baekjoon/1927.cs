ee57e6882d9f7e6805aed61a4412dbfac77a24e6
opza <mingu991114@gmail.com>
2023-07-27 12:16:59 +0900

    20230727

---
+++ Baekjoon/1927.cs
@@ -0,0 +1,19 @@
+﻿using var rs = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+using var ws = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+var n = int.Parse(rs.ReadLine());
+
+var priorityQueue = new PriorityQueue<int, int>();
+for (int i = 0; i < n; i++)
+{
+    var inputValue = int.Parse(rs.ReadLine());
+    if (inputValue > 0)
+        priorityQueue.Enqueue(inputValue, inputValue);
+    else
+    {
+        if (priorityQueue.Count > 0)
+            ws.WriteLine(priorityQueue.Dequeue());
+        else
+            ws.WriteLine("0");
+    }
+}
